f5dd994e6c50127f13cb08dc0e56021b9a6b003e
Jessica Pereira <jcpereira070@gmail.com>
2020-06-19 01:21:26 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,27 +2,3 @@
 def badge_maker(x)
   "Hello, my name is #{x}."
 end
-
-def batch_badge_creator(attendees)
-  attendees.collect do |Jess|
-    badge_maker(Jess)
-  end
-
-end
-
-def assign_rooms(attendees)
-  room = 0
-  attendees.collect do |Jess|
-    room+=1
-    "Hello, #{Jess}! You'll be assigned to room #{room}!"
-  end
-end
-
-def printer(attendees)
-  batch_badge_creator(attendees).each do |value|
-    puts value
-  end
-  assign_rooms(attendees).each do |value|
-    puts value
-  end
-end
