bf4a5096d21a0590a38c7345523fac125e02d8a7
Jessica Pereira <jcpereira070@gmail.com>
2020-06-19 01:33:03 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,3 +5,11 @@ end
 def batch_badge_creator(speakers)
   speakers.collect {|name| badge_maker(name)}
 end
+
+def assign_rooms(speakers)
+  room = 0
+  speakers,collect do |name|
+    room += 1 
+    "Hello, #{name}! You'll be assigned to room #{room}!"
+  end
+end
