38547126af5455bb00f78fc87f540df3c9b202a8
Jessica Pereira <jcpereira070@gmail.com>
2020-06-19 01:43:57 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,14 +1,14 @@
-def badge_maker(x)
-  "Hello, my name is #{x}."
+def badge_maker(a)
+  "Hello, my name is #{a}."
 end
 
-def batch_badge_creator(speakers)
+def batch_badge_creator(speak)
   speakers.collect {|name| badge_maker(name)}
 end
 
-def assign_rooms(speakers)
+def assign_rooms(speak)
   room = 0
-  speakers,collect do |name|
+  speak.collect do |name|
     room += 1 
     "Hello, #{name}! You'll be assigned to room #{room}!"
   end
